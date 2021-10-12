3d72864a550698f20231b750bce7acf2ec7b8650
caiobaraujo <62404862+caiobaraujo@users.noreply.github.com>
2021-10-12 16:58:28 -0300

    update-layout

---
+++ script.js
@@ -0,0 +1,22 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.divisao2');
+let lateral = document.querySelector('.divisao1-right');
+let numeros = document.querySelector('.d-1-3');
+
+function clicou(num) {
+  console.log('clicou o numero: ', num);
+}
+
+function branco() {
+  console.log('clicou em branco');
+}
+
+function corrige() {
+  console.log('clicou em corrige');
+}
+
+function confirma() {
+  console.log('clicou em confirma');
+}
